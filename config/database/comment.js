4c63c778200a3b0e98019a90c275b0866dace711
Meli Uribe <lissauribe21@gmail.com>
2022-12-01 22:58:56 -0500

    FIX MT-02-57

--- config/database/comment.js
+++ config/database/comment.js
@@ -1,26 +1,26 @@
 let comments = [
     {
-        "showId": "63728141a8032917dee94c14",
-        "userId": "636d82c86529ebe93bbef91f",
-        "date": "29-11-2022",
+        "showId": "63728141a8032917dee94c12",
+        "userId": "63896da57c43df3106a39b5b",
+        "date": "2022-09-11",
         "comment": "Im interested on this show. Where can i get more info about it, please?"
     },
     {
         "showId": "63728141a8032917dee94c15",
         "userId": "636d82c86529ebe93bbef91f",
-        "date": "21-11-2022",
+        "date": "2022-10-21",
         "comment": "It's amazing, i would like to go to this show"
     },
     {
         "showId": "63728141a8032917dee94c16",
         "userId": "636d82c86529ebe93bbef922",
-        "date": "09-10-2022",
+        "date": "2023-06-15",
         "comment": "It's amazing, i would like to go to this show"
     },
     {
         "showId": "63728141a8032917dee94c17",
         "userId": "636d82c86529ebe93bbef922",
-        "date": "28-11-2022",
+        "date": "2021-12-21",
         "comment": "wow, i think it's too expensive. I dont like these prices"
     },
 ]
@@ -37,3 +37,4 @@ comments.forEach(element => {
         comment: element.comment
     })
 })
+
